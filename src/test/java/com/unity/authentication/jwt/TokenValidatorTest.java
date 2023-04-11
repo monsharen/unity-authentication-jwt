9b7a1e186558e5dae6da723bf9fa3ed1aa4694e9
monsharen <github@opt.nu>
2023-04-11 14:37:32 +0200

    removed unused log

--- src/test/java/com/unity/authentication/jwt/TokenValidatorTest.java
+++ src/test/java/com/unity/authentication/jwt/TokenValidatorTest.java
@@ -1,7 +1,6 @@
 package com.unity.authentication.jwt;
 
 import com.auth0.jwk.JwkException;
-import lombok.extern.java.Log;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +8,6 @@ import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Log
 public class TokenValidatorTest {
 
     private TokenValidator instance;
